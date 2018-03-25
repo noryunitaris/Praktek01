1d843a44468ac9e86a2ee440f1c6304ec9b84b5a
noryunitaris <yunitarisnaida@gmail.com>
2018-03-25 09:54:53 -0700

    Membuat Atribut dan Behavior

--- src/praktek01/Serangga.java
+++ src/praktek01/Serangga.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author USER
  */
 public class Serangga {
-    
+   String nama;
+   String Makanan;
+   String WaktuAktif;
+   String MasaHidup;
+   String Habitat;
+   
+   void CaraSimbiosis(){
+        System.out.println("Nama Serangga :"+nama);
+        System.out.println("Makanan Serangga :"+Makanan);
+        System.out.println("WAktuAktif Serangga :"+WaktuAktif);
+        System.out.println("MasaHidup Serangga :"+MasaHidup);
+        System.out.println("Habitat Serangga :"+Habitat);
+   }
 }
